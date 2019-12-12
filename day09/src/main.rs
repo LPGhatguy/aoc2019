fc483abf80c15898f3c28fcdcac5c709576c92c2
Lucien Greathouse <me@lpghatguy.com>
2019-12-11 22:05:33 -0800

    Day 9 part 2

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -4,11 +4,25 @@ static INPUT: &str = include_str!("../input.txt");
 
 use intcode::VM;
 
-fn main() {
+fn part_one() {
     let mut vm = VM::default();
     vm.load_memory_from_tape(INPUT);
     vm.put_input(1);
     vm.run_until_terminated();
 
-    println!("{:?}", vm.output);
+    println!("Part one: {:?}", vm.get_output());
+}
+
+fn part_two() {
+    let mut vm = VM::default();
+    vm.load_memory_from_tape(INPUT);
+    vm.put_input(2);
+    vm.run_until_terminated();
+
+    println!("Part two: {:?}", vm.get_output());
+}
+
+fn main() {
+    part_one();
+    part_two();
 }
